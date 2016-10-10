5f656d40ce959d90c7928ece5561ae212b66e9d7
agarcia94 <mattgarcia94@sbcglobal.net>
2016-10-10 11:05:51 -0700

    Added dummy data for list view

--- AlarmNotifier/app/src/main/java/com/example/alexperez/alarmnotifier/MainActivityFragment.java
+++ AlarmNotifier/app/src/main/java/com/example/alexperez/alarmnotifier/MainActivityFragment.java
@@ -1,15 +1,24 @@
 package com.example.alexperez.alarmnotifier;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ListView lv;
+    private ArrayAdapter<String> adapter;
 
     public MainActivityFragment() {
     }
@@ -17,6 +26,21 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        lv = (ListView)rootView.findViewById(R.id.listview);
+
+        ArrayList<String> data = new ArrayList<>();
+        data.add("Los Angeles");
+        data.add("Miami");
+        data.add("Sacramento");
+
+        adapter = new ArrayAdapter(getActivity(), R.layout.row, R.id.textView, data);
+        lv.setAdapter(adapter);
+
+
+
+        return rootView;
     }
 }
